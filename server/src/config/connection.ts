8135af6bd161cb53a02c14a5875ad7d5b993cab2
keenan maupin <maupin.keenan97@gmail.com>
2024-11-14 09:50:41 -0800

    error fix

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import mongoose from 'mongoose';
-
+console.log(process.env);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
 
 export default mongoose.connection;
